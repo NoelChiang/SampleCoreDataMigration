354c1204b36e56e5d7ff48117333e8bb69be6e89
Noel <valkyrie7266@hotmail.com>
2019-01-16 14:05:15 +0800

    Initial Commit

---
+++ SampleCoreDataMigration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleCoreDataMigration
+//
+//  Created by Noel on 2019/1/16.
+//  Copyright © 2019 Noel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
